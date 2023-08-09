8d6df8495c4fff7b286c7384045c56a0a7d57129
Jared Macias <jaredmarcus@icloud.com>
2023-08-09 13:57:21 +0000

    setup frontend structure

---
+++ research-app/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+        <nav>
+            <div className="logo">ResearchApp</div>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Profile</li>
+                <li>Logout</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
